47f86f7be2a680576088b0bb3a9ff66ae7cf1260
Mr.Yogesh <yogeshvanzara98@gmail.com>
2023-03-07 23:31:14 +0530

    some chanes

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "https://fiver-back.onrender.com/api/",
   withCredentials: true,
 });
 
